dfd7d0a784d962321852d7867b7a49e0f5755ece
SantheeshPM <163390587+SantheeshCS@users.noreply.github.com>
2025-09-29 20:16:38 +0530

    Update Api.jsx

--- frontend/src/services/Api.jsx
+++ frontend/src/services/Api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000/api';
+const API_URL = 'https://klat-it-backend.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
